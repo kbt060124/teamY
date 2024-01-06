aedcc13e768656b3c2df55082b5ac0721834c076
root <root@DELL>
2024-01-06 20:43:55 +0900

    Header作成

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,15 +1,18 @@
+import Header from "@/Components/Header";
 import Sidebar from "@/Components/Sidebar";
 import { usePage } from "@inertiajs/react";
 
 const Layout = ({ children }) => {
-
-  const userName = usePage().props.auth.user.name;
-  const userIcon = usePage().props.auth.user.icon;
+    const userName = usePage().props.auth.user.name;
+    const userIcon = usePage().props.auth.user.icon;
 
     return (
-        <div className="flex w-full h-screen">
-            <Sidebar name={userName} icon={userIcon}/>
-            <main>{children}</main>
+        <div>
+            <Header/>
+            <div className="flex w-full h-screen">
+                <Sidebar name={userName} icon={userIcon} />
+                <main>{children}</main>
+            </div>
         </div>
     );
 };
